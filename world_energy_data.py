07d0b7714ebe755426d17c8785cd0ab92648eb68
Shane White <shanewhite@worldenergydata.org>
2024-03-06 10:52:33 +1030

    Add files via upload
    Bar charts of production of all fossil fuels
    Saves charts to files

--- world_energy_data.py
+++ world_energy_data.py
@@ -1,13 +1,9 @@
 import pandas as pd
-import math
 import numpy as np
-import matplotlib
 import matplotlib.pyplot as plt
-import seaborn
-import seaborn.objects as so
 
 
-# conversions
+#define immutable conversions
 class ConstantsNamespace:
     __slots__ = ()
     THOUSAND_TO_MILLION = 1 / 1000
@@ -25,118 +21,136 @@ class ConstantsNamespace:
     COLOR_WIND_SOLAR = '#72F54A'
     COLOR_ELECTRICITY = '#3F8F91'
     FIG_SIZE = 8
-    OIL_PROD_INTERVAL = 20.0
+    OIL_PROD_YAXIS_INTERVAL = 20
+    COAL_PROD_YAXIS_INTERVAL = 1000
+    GAS_PROD_YAXIS_INTERVAL = 1000
 
 
+#import energy institute data from csv file
 def import_energy_inst_data():
-    return(pd.read_csv("Statistical Review of World Energy Narrow File.csv", index_col=['Country'],
-                       usecols=['Country', 'Year', 'Var', 'Value']))
+    return(pd.read_csv(
+        "Statistical Review of World Energy Narrow File.csv",
+        index_col=['Country'],
+        usecols=['Country', 'Year', 'Var', 'Value']))
 
 
-def fossil_fuel_production(data):
-    oil_production = data.loc[data['Var'] == 'oilprod_kbd']
-    oilprod_Mbpd = oil_production.copy()
-    oilprod_Mbpd['Value'] = oilprod_Mbpd['Value'] * constant.THOUSAND_TO_MILLION
+#fossil fuel production charts
+def fossil_fuel_production(profile_country, data):
 
-    # Matplotlib plot:
+    #for chart titles
+    if profile_country == 'Total World':
+        profile_country = 'World'
+
+    #locate production values
+    coalprod_mt = data.loc[data['Var'] == 'coalprod_mt']
+    oilprod_kbd = data.loc[data['Var'] == 'oilprod_kbd']
+    gasprod_bcm = data.loc[data['Var'] == 'gasprod_bcm']
+
+    #coal
     fig = plt.figure(figsize=(constant.FIG_SIZE, constant.FIG_SIZE))
-    ax1 = fig.add_subplot()
-    ax1.bar(oilprod_Mbpd['Year'], oilprod_Mbpd['Value'], width=1, color=constant.COLOR_OIL,
-                             edgecolor='black', linewidth=0.7)
-    ax1.minorticks_off()
-    ax1.tick_params('x', labelrotation = 90)
-    ax1.set_xticks(np.arange(min(oilprod_Mbpd['Year'])+1, max(oilprod_Mbpd['Year'])+1, 2.0))
-    ax1.set_yticks(np.arange(0, max(oilprod_Mbpd['Value'])+constant.OIL_PROD_INTERVAL, constant.OIL_PROD_INTERVAL))
-    ax1.set_title('Oil Production')
-    ax1.set_ylabel('Oil Production (million barrels/day)')
-
-    # Pandas plot:
-    oilprod_Mbpd.plot(
-        kind='bar',
-        x='Year',
-        y='Value',
-        color=constant.COLOR_OIL,
-        title='pandas',
-        edgecolor='black',
+    ax = fig.add_subplot()
+    ax.bar(
+        coalprod_mt['Year'],
+        coalprod_mt['Value'],
         width=1,
-        figsize=(constant.FIG_SIZE, constant.FIG_SIZE))
+        color=constant.COLOR_COAL,
+        edgecolor='grey',
+        linewidth=0.7)
+    ax.tick_params('x', labelrotation = 90)
+    if (float(min(coalprod_mt['Year']) % 2) != 0.0):
+       ax.set_xticks(np.arange(
+           min(coalprod_mt['Year'] + 1),
+           max(coalprod_mt['Year'] + 1),
+           2))
+    else:
+        ax.set_xticks(np.arange(
+            min(coalprod_mt['Year']),
+            max(coalprod_mt['Year'] + 2),
+            2))
+    ax.set_yticks(np.arange(
+        0,
+        max(coalprod_mt['Value']) + constant.COAL_PROD_YAXIS_INTERVAL,
+        constant.COAL_PROD_YAXIS_INTERVAL))
+    title = profile_country + ' Annual Coal Production'
+    ax.set_title(title)
+    ax.set_ylabel('Annual Production (million tonnes (Mt))')
+    plt.savefig(title, dpi=300)
 
-    # Seaborn plot:
-    fig, ax = plt.subplots(figsize=(constant.FIG_SIZE, constant.FIG_SIZE))
-    seaborn.barplot(
-        data=oilprod_Mbpd,
-        x='Year',
-        y='Value',
-        saturation = 1,
+    #oil
+    #create new empty dataframe to accomodate conversion kbpd -> Mpbd
+    oilprod_Mbpd = pd.DataFrame(columns = ['Year', 'Var', 'Value'])
+    oilprod_Mbpd['Year'] = oilprod_kbd['Year']
+    oilprod_Mbpd['Var'] = 'oil_prodMpd'
+    oilprod_Mbpd['Value'] = oilprod_kbd['Value'] * \
+        constant.THOUSAND_TO_MILLION
+    fig = plt.figure(figsize=(constant.FIG_SIZE, constant.FIG_SIZE))
+    ax = fig.add_subplot()
+    ax.bar(
+        oilprod_Mbpd['Year'],
+        oilprod_Mbpd['Value'],
+        width=1,
         color=constant.COLOR_OIL,
         edgecolor='black',
-        width=1)
-    ax.set_title('seaborn')
-    ax.set_ylabel('Annual Production (million barrels per day (Mpbd)')
-    #plt.setp(ax.axes.get_xticklabels(), visible=False)
-    #plt.setp(ax.axes.get_xticklabels()[::5], visible=True)
-    plt.show()
- 
+        linewidth=0.7)
+    ax.tick_params('x', labelrotation = 90)
+    if (float(min(oilprod_Mbpd['Year']) % 2) != 0):
+       ax.set_xticks(np.arange(
+           min(oilprod_Mbpd['Year'] + 1),
+           max(oilprod_Mbpd['Year'] + 1),
+           2))
+    else:
+        ax.set_xticks(np.arange(
+            min(oilprod_Mbpd['Year']),
+            max(oilprod_Mbpd['Year'] + 2),
+            2))
+    ax.set_yticks(np.arange(
+        0,
+        max(oilprod_Mbpd['Value']) + constant.OIL_PROD_YAXIS_INTERVAL,
+        constant.OIL_PROD_YAXIS_INTERVAL))
+    title = profile_country + ' Annual Oil Production'
+    ax.set_title(title)
+    ax.set_ylabel('Annual Production (million barrels/day (Mbpd))')
+    plt.savefig(title, dpi=300)
+    #chart full width if preferred
+    #ax1.set_xlim(min(oilprod_Mbpd['Year']) - 1, max(oilprod_Mbpd['Year']) + 1)
 
+    #gas
+    fig = plt.figure(figsize=(constant.FIG_SIZE, constant.FIG_SIZE))
+    ax = fig.add_subplot()
+    ax.bar(
+        gasprod_bcm['Year'],
+        gasprod_bcm['Value'],
+        width=1,
+        color=constant.COLOR_GAS,
+        edgecolor='black',
+        linewidth=0.7)
+    ax.tick_params('x', labelrotation = 90)
+    if (float(min(gasprod_bcm['Year'] % 2)) != 0):
+       ax.set_xticks(np.arange(
+           min(gasprod_bcm['Year'] + 1),
+           max(gasprod_bcm['Year'] + 1),
+           2))
+    else:
+        ax.set_xticks(np.arange(
+            min(gasprod_bcm['Year']),
+            max(gasprod_bcm['Year'] + 2),
+            2))
+    ax.set_yticks(np.arange(
+        0,
+        max(gasprod_bcm['Value'])+constant.GAS_PROD_YAXIS_INTERVAL,
+        constant.GAS_PROD_YAXIS_INTERVAL))
+    title = profile_country + ' Annual Gas Production'
+    ax.set_title(title)
+    ax.set_ylabel('Annual Production (billion cubic metres (bcm))')
+    plt.savefig(title, dpi=300)
+
+#call functons to source country specific data, and plot charts of that data
 def profile(country):
     ei_country_data = ei_data.loc[country]
-    fossil_fuel_production(ei_country_data)
+    fossil_fuel_production(country, ei_country_data)
 
 
-# world
-# Use non-default matlabplot backend for testing due to macOS Sonoma bug; this causes charts to look pixelated.
-# Enable default version for high res charts but causes program to hang when displaying charts.
-# matplotlib.use('Tkagg')
+#Profile1: World
 constant = ConstantsNamespace()
 ei_data = import_energy_inst_data()
-country = 'Total World'
-profile(country)
-
-# ei_country_data = import_energy_inst_data('Russian Federation')
-# profile(ei_country_data)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# trash
-# oilprod_chart1 = plt.bar(oilprod_Mbpd['Year'], oilprod_Mbpd['Value'], width = 1, color = constant.COLOR_OIL, edgecolor = 'black', linewidth = 0.7)
-# oilprod_chart2 = oilprod_Mbpd.plot.bar(x = 'Year', y = 'Value', color = {constant.COLOR_OIL}, edgecolor = 'black', linewidth = 1)
-# for x in oilprod_Mbpd['Var']:
-#    oilprod_Mbpd['Var'] = 'oilprod_Mbpd'
-# print(oil_production)
-# print(oilprod_Mbpd)
-# oilprod_chart = oilprod_Mbpd.plot.bar(x = 'Year', y = 'Value', color = {constant.COLOR_OIL}, edgecolor = 'black', linewidth = 1)
-#
-# oilprod_chart = plt.bar(oilprod_Mbpd['Year'], oilprod_Mbpd['Value'], width = 1, color = constant.COLOR_OIL, edgecolor = 'black', linewidth = 0.7)
-# for label in oilprod_chart.set_xticklabels.get_ticklabels()[::2]:
-#    label.set_visible(False)
-# for x in oilprod_Mbpd['Year']:
-#    if x % 2 != 0:
-#        year =
-# plt.xticks(oilprod_Mbpd['Year'], oilprod_Mbpd['Year'], rotation = 'vertical')
-# plt.xticks(year, year, rotation = 'vertical')
-# print(year)
-#    ticks=list(range(int(len(oilprod_Mbpd['Year'].tolist())/2)))
-
+profile('Total World')
