e71c3559a631e9eb482821135ca79af9129c129d
Shane White <shanewhite@worldenergydata.org>
2024-03-15 13:07:10 +1030

    Object orientated version of previous

--- world_energy_data.py
+++ world_energy_data.py
@@ -1,156 +1,177 @@
+# Creates charts of national energy systems.
+# Written by Shane White using Python v3.11.5 and Spyder IDE.
+# https://github.com/shanewhi
+# https://www.worldenergydata.org
+
+# Choose a country at bottom of script. If that country has never produced a
+# specific fuel, then a corresponding chart isn't created. Entered country
+# name must match that used by The Energry Institute's dataset.
+
+
+# Import Python modules.
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from enum import Enum
+import inspect
 
 
-#define immutable conversions
+# Define conversions (multiply for conversion).
 class ConstantsNamespace:
     __slots__ = ()
     THOUSAND_TO_MILLION = 1 / 1000
-    COLOR_COAL = '#000000'
-    COLOR_OIL = '#A62B17'
-    COLOR_GAS = '#F09937'
-    COLOR_NUCLEAR = '#F900FF'
-    COLOR_HYDRO = '#479FF8'
-    COLOR_WIND = '#1432F5'
-    COLOR_SOLAR = '#DA3B26'
-    COLOR_GEO_BIO_OTHER = '#92902C'
-    COLOR_OTHER = '#9F6622'
-    COLOR_FOSSIL_FUELS = '#5E5E5E'
-    COLOR_LOW_C = '#53AE32'
-    COLOR_WIND_SOLAR = '#72F54A'
-    COLOR_ELECTRICITY = '#3F8F91'
     FIG_SIZE = 8
-    OIL_PROD_YAXIS_INTERVAL = 20
-    COAL_PROD_YAXIS_INTERVAL = 1000
-    GAS_PROD_YAXIS_INTERVAL = 1000
 
 
-#import energy institute data from csv file
-def import_energy_inst_data():
-    return(pd.read_csv(
-        "Statistical Review of World Energy Narrow File.csv",
-        index_col=['Country'],
-        usecols=['Country', 'Year', 'Var', 'Value']))
+# Define fuel colors for charts.
+class Color(Enum):
+     COAL = '#000000'
+     OIL = '#A62B17'
+     GAS = '#F09937'
+     NUCLEAR = '#F900FF'
+     HYDRO = '#479FF8'
+     WIND = '#1432F5'
+     SOLAR = '#DA3B26'
+     GEO_BIO_OTHER = '#92902C'
+     OTHER = '#9F6622'
+     FOSSIL_FUELS = '#5E5E5E'
+     LOW_C = '#53AE32'
+     WIND_SOLAR = '#72F54A'
+     ELECTRICITY = '#3F8F91'
 
 
-#fossil fuel production charts
-def fossil_fuel_production(profile_country, data):
+# Custom class of country specific energy system.
+class Energy_System:
+    def __init__(
+            self,
+            name, #country name
+            coalprod_Mt,
+            oilprod_Mbpd,
+            gasprod_bcm):
+        self.name = name
+        self.coalprod_Mt = coalprod_Mt
+        self.oilprod_Mbpd = oilprod_Mbpd
+        self.gasprod_bcm = gasprod_bcm
 
-    #for chart titles
-    if profile_country == 'Total World':
-        profile_country = 'World'
+    # Class method charts fuel trends.
+    def chart(energy_system):
+        # Identify attributes in object of custom class Energy_System.
+        a = id_attributes(energy_system)
+        # 1. Fossil fuel production charts.
+        # Identifies production specific attributes by searching for
+        # substring in a list.
+        prod_attrs = [item for item in a if 'prod' in item]
+        for item in prod_attrs:
+            if 'coal' in item and energy_system.coalprod_Mt.empty == False:
 
-    #locate production values
-    coalprod_mt = data.loc[data['Var'] == 'coalprod_mt']
-    oilprod_kbd = data.loc[data['Var'] == 'oilprod_kbd']
-    gasprod_bcm = data.loc[data['Var'] == 'gasprod_bcm']
+                title = energy_system.name + ' Annual Coal Production'
+                ylabel = ('Annual Production (Mt)')
+                Energy_System.column_chart(energy_system.coalprod_Mt,
+                    Color.COAL.value,
+                    title,
+                    ylabel)
+            if 'oil' in item and energy_system.oilprod_Mbpd.empty == False:
+                title = energy_system.name + ' Annual Oil Production'
+                ylabel = ('Annual Production (Mbpd)')
+                Energy_System.column_chart(energy_system.oilprod_Mbpd,
+                    Color.OIL.value,
+                    title,
+                    ylabel)
+            if 'gas' in item and energy_system.gasprod_bcm.empty == False:
+                title = energy_system.name + ' Annual Gas Production'
+                ylabel = ('Annual Production (bcm)')
+                Energy_System.column_chart(energy_system.gasprod_bcm,
+                    Color.GAS.value,
+                    title,
+                    ylabel)
 
-    #coal
-    fig = plt.figure(figsize=(constant.FIG_SIZE, constant.FIG_SIZE))
-    ax = fig.add_subplot()
-    ax.bar(
-        coalprod_mt['Year'],
-        coalprod_mt['Value'],
-        width=1,
-        color=constant.COLOR_COAL,
-        edgecolor='grey',
-        linewidth=0.7)
-    ax.tick_params('x', labelrotation = 90)
-    if (float(min(coalprod_mt['Year']) % 2) != 0.0):
-       ax.set_xticks(np.arange(
-           min(coalprod_mt['Year'] + 1),
-           max(coalprod_mt['Year'] + 1),
-           2))
-    else:
-        ax.set_xticks(np.arange(
-            min(coalprod_mt['Year']),
-            max(coalprod_mt['Year'] + 2),
-            2))
-    ax.set_yticks(np.arange(
-        0,
-        max(coalprod_mt['Value']) + constant.COAL_PROD_YAXIS_INTERVAL,
-        constant.COAL_PROD_YAXIS_INTERVAL))
-    title = profile_country + ' Annual Coal Production'
-    ax.set_title(title)
-    ax.set_ylabel('Annual Production (million tonnes (Mt))')
-    plt.savefig(title, dpi=300)
+    # Class method creates bar chart.
+    def column_chart(data, column_color, title, ylabel):
+        fig = plt.figure(figsize=(constant.FIG_SIZE, constant.FIG_SIZE))
+        ax = fig.add_subplot()
+        if column_color == Color.COAL.value:
+            edge_color = 'grey'
+        else:
+            edge_color = 'black'
+        ax.bar(
+            data['Year'],
+            data['Value'],
+            width=1,
+            color = column_color,
+            edgecolor = edge_color,
+            linewidth=0.7)
+        ax.set_autoscaley_on(True)
+        # On x-axis, display every second year, and display final year
+        # irrespective of whether the start year is odd or even.
+        ax.tick_params('x', labelrotation = 90)
+        if (float(min(data['Year']) % 2) != 0.0):
+            ax.set_xticks(np.arange(
+                min(data['Year'] + 1),
+                max(data['Year'] + 1),
+                2))
+        else:
+            ax.set_xticks(np.arange(
+                min(data['Year']),
+                max(data['Year'] + 2),
+                2))
+        ax.set_title(title)
+        ax.set_ylabel(ylabel)
+        #plt.savefig(title, dpi=300)
 
-    #oil
-    #create new empty dataframe to accomodate conversion kbpd -> Mpbd
+#End custom class Energy_System.
+
+
+# Function extracts attributes from instance of Energy_System custom class.
+def id_attributes(instance):
+    # create empty list to hold attributes
+    attr_list = []
+    # iterate through instance attributes and exclude non-custom attributes
+    for attr, value in inspect.getmembers(instance):
+        if not inspect.ismethod(value) and not attr.startswith('__'):
+            attr_list.append(attr)
+            # uncomment next line to append values
+            # attr_list.append(value)
+    return(attr_list)
+
+
+# Function extracts country specific data from Energy Institute's dataset.
+# Returns instance of custom class Energy_System.
+def populate_energy_system(country):
+    country_data = ei_data.loc[country]
+    coalprod_Mt = country_data.loc[country_data['Var'] == 'coalprod_mt']
+    oilprod_kbpd = country_data.loc[country_data['Var'] == 'oilprod_kbd']
+    gasprod_bcm = country_data.loc[country_data['Var'] == 'gasprod_bcm']
+    #create new empty pandas dataframe for oil unit conversion kbpd -> Mpbd
     oilprod_Mbpd = pd.DataFrame(columns = ['Year', 'Var', 'Value'])
-    oilprod_Mbpd['Year'] = oilprod_kbd['Year']
-    oilprod_Mbpd['Var'] = 'oil_prodMpd'
-    oilprod_Mbpd['Value'] = oilprod_kbd['Value'] * \
+    oilprod_Mbpd['Year'] = oilprod_kbpd['Year']
+    oilprod_Mbpd['Var'] = 'oilprod_Mbpd'
+    oilprod_Mbpd['Value'] = oilprod_kbpd['Value'] * \
         constant.THOUSAND_TO_MILLION
-    fig = plt.figure(figsize=(constant.FIG_SIZE, constant.FIG_SIZE))
-    ax = fig.add_subplot()
-    ax.bar(
-        oilprod_Mbpd['Year'],
-        oilprod_Mbpd['Value'],
-        width=1,
-        color=constant.COLOR_OIL,
-        edgecolor='black',
-        linewidth=0.7)
-    ax.tick_params('x', labelrotation = 90)
-    if (float(min(oilprod_Mbpd['Year']) % 2) != 0):
-       ax.set_xticks(np.arange(
-           min(oilprod_Mbpd['Year'] + 1),
-           max(oilprod_Mbpd['Year'] + 1),
-           2))
-    else:
-        ax.set_xticks(np.arange(
-            min(oilprod_Mbpd['Year']),
-            max(oilprod_Mbpd['Year'] + 2),
-            2))
-    ax.set_yticks(np.arange(
-        0,
-        max(oilprod_Mbpd['Value']) + constant.OIL_PROD_YAXIS_INTERVAL,
-        constant.OIL_PROD_YAXIS_INTERVAL))
-    title = profile_country + ' Annual Oil Production'
-    ax.set_title(title)
-    ax.set_ylabel('Annual Production (million barrels/day (Mbpd))')
-    plt.savefig(title, dpi=300)
-    #chart full width if preferred
-    #ax1.set_xlim(min(oilprod_Mbpd['Year']) - 1, max(oilprod_Mbpd['Year']) + 1)
+    if country == 'Total World':
+        country = 'World'
+    return (Energy_System(
+        country,
+        coalprod_Mt,
+        oilprod_Mbpd,
+        gasprod_bcm))
 
-    #gas
-    fig = plt.figure(figsize=(constant.FIG_SIZE, constant.FIG_SIZE))
-    ax = fig.add_subplot()
-    ax.bar(
-        gasprod_bcm['Year'],
-        gasprod_bcm['Value'],
-        width=1,
-        color=constant.COLOR_GAS,
-        edgecolor='black',
-        linewidth=0.7)
-    ax.tick_params('x', labelrotation = 90)
-    if (float(min(gasprod_bcm['Year'] % 2)) != 0):
-       ax.set_xticks(np.arange(
-           min(gasprod_bcm['Year'] + 1),
-           max(gasprod_bcm['Year'] + 1),
-           2))
-    else:
-        ax.set_xticks(np.arange(
-            min(gasprod_bcm['Year']),
-            max(gasprod_bcm['Year'] + 2),
-            2))
-    ax.set_yticks(np.arange(
-        0,
-        max(gasprod_bcm['Value'])+constant.GAS_PROD_YAXIS_INTERVAL,
-        constant.GAS_PROD_YAXIS_INTERVAL))
-    title = profile_country + ' Annual Gas Production'
-    ax.set_title(title)
-    ax.set_ylabel('Annual Production (billion cubic metres (bcm))')
-    plt.savefig(title, dpi=300)
 
-#call functons to source country specific data, and plot charts of that data
+# Function creates profile of a country's energy system by creating an
+# instance of custom class Energy_System, populating that object with data,
+# and creating charts.
 def profile(country):
-    ei_country_data = ei_data.loc[country]
-    fossil_fuel_production(country, ei_country_data)
+    country_energy_system = populate_energy_system(country)
+    Energy_System.chart(country_energy_system)
 
 
-#Profile1: World
+# Main function.
 constant = ConstantsNamespace()
-ei_data = import_energy_inst_data()
+ei_data = pd.read_csv(
+    "Statistical Review of World Energy Narrow File.csv",
+    index_col=['Country'],
+    usecols=['Country', 'Year', 'Var', 'Value'])
+# Create energy system charts for the following countries:
 profile('Total World')
+profile('Germany')
+profile('United Arab Emirates')
+profile('United Kingdom')
