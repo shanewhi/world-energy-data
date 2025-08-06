7c15340d17708c067fa0934da6da211209074348
Shane White <whitesha@protonmail.com>
2025-08-06 13:57:59 +0930

    Minor change
    Added country name to per capita chart.

--- output.py
+++ output.py
@@ -458,9 +458,9 @@ https://www.energyinst.org/statistical-review/resources-and-data-downloads.'
 ########################################################################################################################
 def per_capita_emissions(energy_system):
     fig_dir = 'charts ' + energy_system.country + '/'
-    os.makedirs(fig_dir, exist_ok=True)  # Save co2 charts in this directory.
-    title1 = 'Per Capita Fossil Fuel CO\u2082 Emissions'
-    title2 = 'Year ' + str(energy_system.pc_associated_data['FY'])
+    os.makedirs(fig_dir, exist_ok=True)  # Save CO2 charts in this directory.
+    country_name_yr = str(energy_system.country) + ', Year ' + str(energy_system.pc_associated_data['FY'])
+    title = 'Per Capita Fossil Fuel CO\u2082 Emissions'
     footer_text = ("Data: Population - World Bank Group, dataset 'Population, total', "
                    + "https://data.worldbank.org/indicator/SP.POP.TOTL. Fossil fuel CO\u2082 emissions - "
                    + "The Energy Institute Statistical Review of World Energy, "
@@ -503,7 +503,7 @@ def per_capita_emissions(energy_system):
                    )
 
     ylabel = 'Tonne'
-    chart.column_subplot(title1, title2, ylabel, footer_text, user_globals.Color.CO2_EMISSION.value,
+    chart.column_subplot(country_name_yr, title, ylabel, footer_text, user_globals.Color.CO2_EMISSION.value,
                          user_globals.Color.PER_CAPITA_HIGHLIGHT.value, energy_system.pc_tco2,
                          energy_system.country_pc_tco2)
     plt.savefig(
