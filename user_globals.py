5c9940dc30bd9e08dbe2c705844887bbf987798e
Shane White <whitesha@protonmail.com>
2025-08-07 22:20:35 +0930

    Changed value of per capita threshold to 0

--- user_globals.py
+++ user_globals.py
@@ -107,7 +107,7 @@ class Constant(Enum):
     # fossil fuel CO2. Such countries are designated individual tiles and have their name displayed in the treemap of
     # fossil fuel CO2 emissions by country share, and have their fossil fuel primary energy trends plotted and saved in
     # the Major Emitters folder.
-    PER_CAPITA_THRESHOLD = 5  # tCO2/per-capita. Values greater than or equal to this threshold are plotted individually
+    PER_CAPITA_THRESHOLD = 0  # tCO2/per-capita. Values greater than or equal to this threshold are plotted individually
     # and the remainder are plotted as 'Other'.
     COAL_SHARE_RANK_THRESHOLD = 5  # Percent of global coal production. Defines large coal producer.
     OIL_SHARE_RANK_THRESHOLD = 2  # Percent of global oil production. Defines large oil producer.
